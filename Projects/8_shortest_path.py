3b4651ffb32a258b84592ecea973f9e607cc7d94
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2025-03-21 19:11:47 +0100

    Update 8_shortest_path.py

--- Projects/8_shortest_path.py
+++ Projects/8_shortest_path.py
@@ -8,16 +8,26 @@ my_graph = {
 }
 
 def shortest_path(graph, start, target = ''):
+    #Unvisited nodes
     unvisited = list(graph)
+    #Distances from starting node to X node
     distances = {node: 0 if node == start else float('inf') for node in graph}
+    #Paths from starting node to X node
     paths = {node: [] for node in graph}
     paths[start].append(start)
-    
+
+    #The while continues until all the nodes have been visited
     while unvisited:
+        #Choses the current node based on the minimum distance from the starting node
         current = min(unvisited, key=distances.get)
+        #Checks neighboring nodes of the current node
         for node, distance in graph[current]:
+            #If the distance between current node-neighboring node + distance current node-starting node is
+            #less than the current known distance starting node-neighboring node
+            #the minimum distance between starting node and neighboring node get updated
             if distance + distances[current] < distances[node]:
                 distances[node] = distance + distances[current]
+                #If a shorter distance than previously known to node is found path[node] gets updated
                 if paths[node] and paths[node][-1] == node:
                     paths[node] = paths[current][:]
                 else:
